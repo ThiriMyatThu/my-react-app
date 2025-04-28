86ddfa069c7050084afc909f5df61f2628f656af
Thiri Myat Thu <thitimyatthu.y2k3@gmail.com>
2025-04-28 22:53:21 +0700

    "adding m2 hook"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import RegisterForm from "./m2_hook";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <Link to="/RegisterForm" className="nav-item">Module 2 _ Hooks</Link>
+
+      </nav>
+      <Routes>
+        <Route path="/" element={<h1>Welcome Home!</h1>} />
+        <Route path="/register" element={<RegisterForm />} />
+      </Routes>
+
     </div>
   );
-}
+};
 
 export default App;
